e41e54027c8649bd22955a551d3b1fbd562f0cb7
Osadebe Chijioke Akaolisa <osadebec98@gmail.com>
2022-03-23 12:54:00 +0100

    Interactivity

--- canvas.js
+++ canvas.js
@@ -1,12 +1,20 @@
 var canvas = document.querySelector("canvas");
-document.onmousemove = function (e) {
-  var x = e.pageX;
-  var y = e.pageY;
-  console.log(`X: ${x}\nY:${y}`);
-};
+// document.onmousemove = function (e) {
+//   var x = e.pageX;
+//   var y = e.pageY;
+//   console.log(`X: ${x}\nY:${y}`);
+// };
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+let mouse = {
+  x: undefined,
+  y: undefined,
+};
+const maxRadius = 40;
+const minRadius = 2;
+const colorArray = ["red", "yellow", "blue"];
 
 const c = canvas.getContext("2d");
 
@@ -45,11 +53,18 @@ const c = canvas.getContext("2d");
 
 // Creating Arcs
 
+window.addEventListener("mousemove", (event) => {
+  // console.log(event);
+  mouse.x = event.x;
+  mouse.y = event.y;
+  console.log(mouse);
+});
+
 let x = Math.random() * innerWidth;
 let dx = (Math.random() - 0.5) * 8;
 let y = Math.random() * innerHeight;
 let dy = (Math.random() - 0.5) * 8;
-let radius = 30;
+let radius = Math.random() * 3 + 1;
 
 const Circle = function (x, y, dx, dy, radius) {
   this.x = x;
@@ -57,11 +72,14 @@ const Circle = function (x, y, dx, dy, radius) {
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
+  this.color = colorArray[Math.floor(Math.random() * 3.9)];
   this.draw = function () {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     c.strokeStyle = "blue";
-    c.stroke();
+    c.fillStyle = this.color;
+    // c.stroke();
+    c.fill();
   };
   this.update = function () {
     this.y += this.dy;
@@ -74,6 +92,19 @@ const Circle = function (x, y, dx, dy, radius) {
     }
     if (this.x - this.radius < 0) {
       this.dx = -this.dx;
+    }
+
+    if (
+      mouse.x - this.x < 50 &&
+      mouse.x - this.x > -50 &&
+      mouse.y - this.y < 50 &&
+      mouse.y - this.y > -50
+    ) {
+      if (this.radius < maxRadius) {
+        this.radius += 1;
+      }
+    } else if (this.radius > minRadius) {
+      this.radius -= 1;
     }
     this.draw();
   };
